75ae9d19a85a4732e158b8ae80e4d172f37f12a4
Rocio Velazquez <rocyvelazquez2021@gmail.com>
2024-02-27 19:58:55 -0300

    se implemento CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,42 @@ const candidatesRoutes = require('./routes/api/candidates.js');
 
 app.listen(PORT, () => {console.log('El servidor esta corriendo en el puerto: ' + PORT);
 });
+app.use('/uploads', express.static(path.join(__dirname, '../public/img')));
+/**
+ * CORS: es un paquete que simplifica la configuración de políticas de acceso cruzado (CORS) en 
+ * Express.
+ */
+const cors = require("cors");
+
+/**
+ * Especifica que se permite el acceso desde cualquier origen (origin: "*")
+ * Esto permite que cualquier dominio realice solicitudes al servidor
+ */
+let corsOptions = {
+  origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+/**
+ * Esto define una función llamada allowCrossDomain, que actúa como un middleware personalizado.
+ * La función toma tres argumentos: req (la solicitud), res (la respuesta) y next (una función que
+ * permite pasar la solicitud al siguiente middleware
+ */
+let allowCrossDomain = function(req, res, next) {
+  /**
+   *  Se establecen varias cabeceras de respuesta (res.header) para permitir que diferentes dominios 
+   *  realicen solicitudes a tu servido
+   */
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header("Access-Control-Allow-Methods", "OPTIONS, POST, GET, PUT, DELETE");
+    res.header('Access-Control-Allow-Headers', "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+    next();
+  }
+
+  // Se ejecuta el middleware creado
+app.use(allowCrossDomain);
 
 app.get('/', (req, res) => {
     res.send('hola mundo');
