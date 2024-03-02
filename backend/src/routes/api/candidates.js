40d8399132c8eeac69732ff4e49f10bc7dbd4457
Jorge02rl <jorgerodrigo2598mail.com>
2024-03-02 11:21:27 -0300

    cambios en card.jsx

--- backend/src/routes/api/candidates.js
+++ backend/src/routes/api/candidates.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const candidateController= require("../../controllers/api/candidateController");
+
 router.get('/all', candidateController.all);
 
 module.exports = router;
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import banner from '../assets/img/banner.jpg'
 import gloria from '../assets/img/foto1.jpg'
 import daniel from '../assets/img/foto2.jpg'
@@ -10,6 +11,29 @@ import luis from '../assets/img/foto6.jpg'
 
 function Card(){
 
+	const [candidate, setCandidate] = useState({});
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch('http://localhost:3001/candidate/all');
+
+				if(!response.ok){
+					throw new Error('Petición fallida');
+				}
+
+				const data = await response.json();
+				console.log(data);
+
+				setCandidate(data.data);
+				/* console.log(candidate); */
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		}
+		console.log(candidate);
+		fetchData();
+	}, []);
 
     return(
         <>
@@ -19,126 +43,27 @@ function Card(){
 			<section className="content aspirantes">
 				<h2>Aspirantes</h2>
 				<article className="person-boxes">
-					<div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-						<div className="box-avatar">
-							<img src={gloria} alt="Gloria"/>
+
+						<div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+							<div className="box-avatar">
+								<img src={gloria} alt="Gloria"/>
+							</div>
+							<div className="box-bio">
+								<h2 className="bio-name">{candidate[0].name}</h2>
+								<p className="bio-position">Administrador</p>
+							</div>
+							<div className="box-actions">
+								<button>
+									<i className="bi bi-star"></i>
+								</button>
+								<button>
+									<i className="bi bi-chat"></i>
+								</button>
+								<button>
+									<i className="bi bi-envelope"></i>
+								</button>
+							</div>
 						</div>
-						<div className="box-bio">
-							<h2 className="bio-name">Gloria Medina</h2>
-							<p className="bio-position">Administrador</p>
-						</div>
-						<div className="box-actions">
-							<button>
-								<i className="bi bi-star"></i>
-							</button>
-							<button>
-								<i className="bi bi-chat"></i>
-							</button>
-							<button>
-								<i className="bi bi-envelope"></i>
-							</button>
-						</div>
-					</div>
-					<div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-						<div className="box-avatar">
-							<img src={daniel} alt="Daniel Fuentes"/>
-						</div>
-						<div className="box-bio">
-							<h2 className="bio-name">Daniel Fuentes</h2>
-							<p className="bio-position">Técnico de sonido</p>
-						</div>
-						<div className="box-actions">
-							<button>
-								<i className="bi bi-star"></i>
-							</button>
-							<button>
-								<i className="bi bi-chat"></i>
-							</button>
-							<button>
-								<i className="bi bi-envelope"></i>
-							</button>
-						</div>
-					</div>
-					<div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-						<div className="box-avatar">
-							<img src={lee} alt="Lee Chim"/>
-						</div>
-						<div className="box-bio">
-							<h2 className="bio-name">Tim Tim</h2>
-							<p className="bio-position">Linguista</p>
-						</div>
-						<div className="box-actions">
-							<button>
-								<i className="bi bi-star"></i>
-							</button>
-							<button>
-								<i className="bi bi-chat"></i>
-							</button>
-							<button>
-								<i className="bi bi-envelope"></i>
-							</button>
-						</div>
-					</div>
-					<div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-						<div className="box-avatar">
-							<img src={rocio} alt="Rocio"/>
-						</div>
-						<div className="box-bio">
-							<h2 className="bio-name">Rocio Carle</h2>
-							<p className="bio-position">Profesor</p>
-						</div>
-						<div className="box-actions">
-							<button>
-								<i className="bi bi-star"></i>
-							</button>
-							<button>
-								<i className="bi bi-chat"></i>
-							</button>
-							<button>
-								<i className="bi bi-envelope"></i>
-							</button>
-						</div>
-					</div>
-					<div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-						<div className="box-avatar">
-							<img src={victor} alt="Victor"/>
-						</div>
-						<div className="box-bio">
-							<h2 className="bio-name">Victor Fuentes</h2>
-							<p className="bio-position">Computista</p>
-						</div>
-						<div className="box-actions">
-							<button>
-								<i className="bi bi-star"></i>
-							</button>
-							<button>
-								<i className="bi bi-chat"></i>
-							</button>
-							<button>
-								<i className="bi bi-envelope"></i>
-							</button>
-						</div>
-					</div>
-					<div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
-						<div className="box-avatar">
-							<img src={luis} alt="Luis"/>
-						</div>
-						<div className="box-bio">
-							<h2 className="bio-name">Luis Fuentes</h2>
-							<p className="bio-position">Economista</p>
-						</div>
-						<div className="box-actions">
-							<button>
-								<i className="bi bi-star"></i>
-							</button>
-							<button>
-								<i className="bi bi-chat"></i>
-							</button>
-							<button>
-								<i className="bi bi-envelope"></i>
-							</button>
-						</div>
-					</div>
 				</article>
 			</section>
 			
